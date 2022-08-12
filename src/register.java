5e949d5afdf611157a923838bac9ca9b1d05b2a7
emma1 <emma.12030@outlook.com>
2022-08-12 20:46:34 +0700

    debugging on user input (login function hasn't finished yet since I'm still having trouble with the read file as 2d array)

--- src/register.java
+++ src/register.java
@@ -1,11 +1,34 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class register {
     private String firstName;
     private String lastName;
     private String userEmail;
     private String userPassword;
+    private String userStatus;
+    private String userID;
+
+
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+
+
 
     public String getFirstName() {
         return firstName;
@@ -39,66 +62,137 @@ public class register {
         this.userPassword = userPassword;
     }
 
-    public register(String firstName, String lastName, String userEmail, String userPassword){
+
+    public register(String firstName, String lastName, String userEmail, String userPassword, String userStatus, String userID){
         this.firstName = firstName;
         this.lastName = lastName;
         this.userEmail = userEmail;
         this.userPassword = userPassword;
+        this.userStatus = userStatus;
+        this.userID = userID;
     }
-    public static void userRegister() {
+    public static void userRegister() throws IOException {
+        //user input
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your first name: ");
         String firstName = scanner.nextLine();
-        checkValidName(firstName);
+        firstName = checkValidName(firstName);
+
         System.out.print("Enter your last name: ");
         String lastName = scanner.nextLine();
-        checkValidName(lastName);
+        lastName = checkValidName(lastName);
+
         System.out.print("Enter your email: ");
         String userEmail = scanner.nextLine();
-        while (!userEmail.matches("^(.+)@(\\S+)$")) {
-            System.out.println("Invalid email");
-        }
+        userEmail = checkEmailSyntax(userEmail);
+        userEmail = checkAvailable(userEmail);
+
         System.out.println("Enter your password (At least 8 characters)");
         String userPassword = scanner.nextLine();
-        while (!userEmail.matches(".{8,}$"))
-        {
-            System.out.println("Password too short, re-enter please ");
-        }
-        System.out.println("Re-Enter your password ");
-        String reuserPassword = scanner.nextLine();
-        while (!userPassword.equals(reuserPassword))
-        {
-            System.out.println("Password not match");
-        }
+        userPassword = checkPassword(userPassword);
+
+        String userStatus = "new member";
+
+        //user confirmation
         System.out.println("All correct? Click 1 to finish your registration, 2 to cancel");
         int userChoice = scanner.nextInt();
-        if (userChoice ==1 ) {
+        //store their information if they agreed
+        if (userChoice == 1) {
+            //generate userID
+            UUID uuid = UUID.randomUUID();
+            String userID = uuid.toString();
+            //store data
             try (FileWriter fw = new FileWriter("userdata.txt", true);
                  BufferedWriter bw = new BufferedWriter(fw);
-                 PrintWriter out = new PrintWriter(bw)) {
-                out.println(firstName + "," + lastName + "," + userEmail + "," + userPassword);
+                 PrintWriter pw = new PrintWriter(bw)) {
+
+                pw.println(firstName + "," + lastName + "," + userEmail + "," + userPassword + "," + userStatus +","+ userID);
+                pw.flush();
 
             } catch (IOException e) {
-                //exception handling
+                System.out.println("Error file cannot found");
             }
             System.out.println("Thank you! You have register to our site");
-        }
-        else if (userChoice ==2)
-        {
+        } else if (userChoice == 2) {
             System.out.println("Thank you for visiting our site");
         }
 
-
     }
-    public static void checkValidName(String input)
+
+    //method
+    public static String checkValidName(String input)
     {
         while (!input.matches("[a-zA-Z]+"))
         {
-            System.out.println("Invalid input");
+            System.out.println("Invalid input, try again");
+            Scanner scanner =new Scanner(System.in);
+            input = scanner.nextLine();
+
         }
+        return input;
 
     }
+    public static String checkAvailable(String input) throws IOException {
+        String fileName = "userdata.csv";
+        File file = new File(fileName);
+        FileReader fr;
+        try {
+            fr = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] values = line.split(",");
+            if (input.equals(values[2])&&!input.isEmpty()) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("This email is available, try again");
+                input = scanner.nextLine();
+                checkEmailSyntax(input);
 
+            }
 
+        }
+        return input;
+    }
+    public static String checkEmailSyntax(String input)
+    {
+        while (!input.matches("^(.+)@(\\S+)$")) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Invalid email, try again");
+            input = scanner.nextLine();
+
+        }
+        return input;
+    }
+
+    //            public static void userStatus()
+//            {
+//
+//            }
+    public static String checkPassword(String input)
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (!input.matches(".{8,}$"))
+        {
+            System.out.println("Password too short, re-enter please ");
+            input = scanner.nextLine();
+        }
+        System.out.println("Re-Enter your password ");
+        String reuserPassword = scanner.nextLine();
+        while (!input.equals(reuserPassword))
+        {
+            System.out.println("Password not match. Try again");
+            reuserPassword = scanner.nextLine();
+        }
+
+        return input;
+
+    }
 }
+
+
+
 
