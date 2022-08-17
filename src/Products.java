57d0d569727233abbe5d6f223a1c1f54d66309c3
nhattran1206 <s3924826@rmit.edu.vn>
2022-08-17 12:05:01 +0700

    update product and delete product based on id

--- src/Products.java
+++ src/Products.java
@@ -1,5 +1,8 @@
 import java.util.*;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 public class Products {
     private String id;
     private String category;
@@ -79,7 +82,7 @@ public class Products {
                 pw.close();
             }
             catch(Exception e){}
-            System.out.println("Do you want to add more products to the storage?(Yes:1/No:0)");
+            System.out.println("Do you want to add more products to the storage?(Yes: 1/No: 0)");
             exit = sc.nextInt();
         }
 
@@ -99,5 +102,110 @@ public class Products {
             }
         }
         catch(Exception e){}
+    }
+    public void updateProduct()
+    {
+        /////////phase 1: Identifying rows to update
+        System.out.println("Enter the ID of product that you want to update.....");
+        Scanner sc=new Scanner(System.in);
+        int id_to_change=sc.nextInt();
+        String to_change_column_names[]={"Category:","Name:","Quantity:","Price:"};
+        int yes_no[]=new int[4];
+        String to_update[]=new String[4];
+
+        System.out.println("Select the columns that you want to update (Yes: 1/No: 0)");
+        //Loop 4 times = 4 columns to confirm which is going to be updated
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(to_change_column_names[i]+"");
+            int temp=sc.nextInt();
+            yes_no[i]=temp;
+        }
+
+        ////////////////phase2: Updating values
+        System.out.println("_______________________________________________________");
+        System.out.println("Add new values to the columns");
+        //Verifying which columns can be updated
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(to_change_column_names[i]+"");
+            if(yes_no[i]==1)
+            {
+                Scanner sc1=new Scanner(System.in);
+                String temp_val=sc1.nextLine();
+                to_update[i]=temp_val;
+                System.out.println();
+            }
+            else
+            {
+                System.out.println("Cannot be changed");
+            }
+        }
+
+        //////////////////phase 3: Write into file updated values
+        StringBuffer sb=new StringBuffer();
+        try
+        {
+            BufferedReader br=new BufferedReader(new FileReader("Products.txt"));
+            String s;
+            while((s=br.readLine())!=null)
+            {
+                String data[];
+                data=s.split(",");
+                if(id_to_change==Integer.parseInt(data[0]))
+                {
+                    String row=data[0]+",";
+                    //Going through 4 columns if 1 add updated value from phase 2, else keep moving on
+                    for(int i=0;i<4;i++)
+                    {
+                        if(yes_no[i]==1)
+                        {
+                            row=row+to_update[i]+",";
+                        }
+                        else
+                        {
+                            row=row+data[i+1]+",";
+                        }
+
+                    }
+                    sb.append(row);
+                    sb.append("\n");
+                }
+                else
+                {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            }
+            ////////////////// phase 4: Show the updated information
+            File f=new File("Products.txt");
+            PrintWriter pw=new PrintWriter(new FileOutputStream(f,false));
+            pw.print(sb.toString());
+            pw.close();
+        }
+        catch(Exception e) {}
+    }
+    public void deleteProduct(){
+        System.out.println("Enter the ID of product that you want to delete.....");
+        Scanner sc=new Scanner(System.in);
+        int id_to_change=sc.nextInt();
+        try {
+            File inputFile = new File("Products.txt");
+            File tempFile = new File("myTempFile.txt");
+
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+            String s;
+            while((s=reader.readLine())!=null) {
+                String data[] = s.split(",");
+                String id = data[0];
+                if (!id.equals(id_to_change));
+            }
+            writer.close();
+            reader.close();
+            inputFile.delete();
+            tempFile.renameTo(inputFile);
+        }
+        catch(Exception e) {}
     }
 }
