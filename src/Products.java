fcebb0a089356c0a011c7bf8727326747a31d158
nhattran1206 <s3924826@rmit.edu.vn>
2022-08-18 21:40:56 +0700

    big code modification

--- src/Products.java
+++ src/Products.java
@@ -4,18 +4,20 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 public class Products {
-    private String id;
+    private int id;
     private String category;
     private String name;
-    private String quantity;
-    private String price;
+    private int quantity;
+    private int price;
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -35,24 +37,24 @@ public class Products {
         this.name = name;
     }
 
-    public String getQuantity() {
+    public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(String quantity) {
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public String getPrice() {
+    public int getPrice() {
         return price;
     }
 
-    public void setPrice(String price) {
+    public void setPrice(int price) {
         this.price = price;
     }
 
 
-    public Products(String id, String category, String name, String quantity, String price) {
+    public Products(int id, String category, String name, int quantity, int price) {
         this.id = id;
         this.category = category;
         this.name = name;
@@ -61,194 +63,17 @@ public class Products {
 
     }
 
-    public void addProduct() {
-        Scanner sc = new Scanner(System.in);
-        int exit = 1;
-        while(exit!=0) {
-            System.out.println("Enter id of product:");
-            id = sc.next();
-            System.out.println("Enter category:");
-            category = sc.next();
-            System.out.println("Enter name:");
-            name = sc.next();
-            System.out.println("Enter quantity:");
-            quantity = sc.next();
-            System.out.println("Enter price:");
-            price = sc.next();
-
-            try{
-                File f = new File("Products.txt");
-                PrintWriter pw = new PrintWriter(new FileOutputStream(f, true));
-                pw.append("\n"+id+","+category+","+name+","+quantity+","+price);
-                pw.close();
-            }
-            catch(Exception e){}
-            System.out.println("Do you want to add more products to the storage?(Yes: 1/No: 0)");
-            exit = sc.nextInt();
-        }
-
+    @Override
+    public String toString() {
+        return "Products{" +
+                "id='" + id + '\'' +
+                ", category='" + category + '\'' +
+                ", name='" + name + '\'' +
+                ", quantity='" + quantity + '\'' +
+                ", price='" + price + '\'' +
+                '}';
     }
-    public void showProduct() {
-        try
-        {
-            BufferedReader br = new BufferedReader(new FileReader("Products.txt"));
-            String s = "";
-            while ((s = br.readLine()) != null) {
-                String data[] = new String[5];
-                data = s.split(",");
-                for (int i = 0; i < 5; i++) {
-                    System.out.println(data[i] + " ");
-                }
-                System.out.println();
-            }
-        }
-        catch(Exception e){}
-    }
-    public void updateProduct()
-    {
-        /////////phase 1: Identifying rows to update
-        System.out.println("Enter the ID of product that you want to update.....");
-        Scanner sc=new Scanner(System.in);
-        int id_to_change=sc.nextInt();
-        String to_change_column_names[]={"Category:","Name:","Quantity:","Price:"};
-        int yes_no[]=new int[4];
-        String to_update[]=new String[4];
 
-        System.out.println("Select the columns that you want to update (Yes: 1/No: 0)");
-        //Loop 4 times = 4 columns to confirm which is going to be updated
-        for(int i=0;i<4;i++)
-        {
-            System.out.println(to_change_column_names[i]+"");
-            int temp=sc.nextInt();
-            yes_no[i]=temp;
-        }
 
-        ////////////////phase2: Updating values
-        System.out.println("_______________________________________________________");
-        System.out.println("Add new values to the columns");
-        //Verifying which columns can be updated
-        for(int i=0;i<4;i++)
-        {
-            System.out.println(to_change_column_names[i]+"");
-            if(yes_no[i]==1)
-            {
-                Scanner sc1=new Scanner(System.in);
-                String temp_val=sc1.nextLine();
-                to_update[i]=temp_val;
-                System.out.println();
-            }
-            else
-            {
-                System.out.println("Cannot be changed");
-            }
-        }
-
-        //////////////////phase 3: Write into file updated values
-        StringBuffer sb=new StringBuffer();
-        try
-        {
-            BufferedReader br=new BufferedReader(new FileReader("Products.txt"));
-            String s;
-            while((s=br.readLine())!=null)
-            {
-                String data[];
-                data=s.split(",");
-                if(id_to_change==Integer.parseInt(data[0]))
-                {
-                    String row=data[0]+",";
-                    //Going through 4 columns if 1 add updated value from phase 2, else keep moving on
-                    for(int i=0;i<4;i++)
-                    {
-                        if(yes_no[i]==1)
-                        {
-                            row=row+to_update[i]+",";
-                        }
-                        else
-                        {
-                            row=row+data[i+1]+",";
-                        }
-
-                    }
-                    sb.append(row);
-                    sb.append("\n");
-                }
-                else
-                {
-                    sb.append(s);
-                    sb.append("\n");
-                }
-            }
-            ////////////////// phase 4: Show the updated information
-            File f=new File("Products.txt");
-            PrintWriter pw=new PrintWriter(new FileOutputStream(f,false));
-            pw.print(sb.toString());
-            pw.close();
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void deleteProduct(){
-        System.out.println("Enter the name of product that you want to delete.....");
-        Scanner sc=new Scanner(System.in);
-        String name_to_change=sc.next();
-        try {
-            File inputFile = new File("Products.txt");
-            File tempFile = new File("myTempFile.txt");
-
-            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-            String s;
-            while((s=reader.readLine())!=null) {
-                String data[] = s.split(",");
-                String name = data[2];
-                //Only write to new file where the name does not match the input name
-                if (!name.equals(name_to_change)){
-                    writer.write(s + "\n");
-                }
-            }
-            writer.close();
-            reader.close();
-            inputFile.delete();
-            tempFile.renameTo(inputFile);
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void showProductByCategory(){
-        ArrayList<String> records = new ArrayList<>();
-        //create a boolean variable to confirm if the category is available or not
-        boolean found = false;
-        System.out.println("Please enter the category of product that you want to see...");
-        Scanner sc =new Scanner(System.in);
-        String searchCategory= sc.next();
-        try{
-            BufferedReader reader = new BufferedReader(new FileReader("Products.txt"));
-            String s;
-            //Read each line in the Products.txt file
-            while((s=reader.readLine())!=null) {
-                String data[] = s.split(",");
-                String category = data[1];
-                //add that line to Arraylist if the category matches
-                if (category.equals(searchCategory)){
-                    records.add(s);
-                    found = true;
-                }
-            }
-            reader.close();
-            //if found is still false then category unavailable
-            if(!found){
-                System.out.println("No records found");
-            }
-
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-        for(String str: records){
-            System.out.println(str);
-        }
-    }
 
 }
---
+++ src/ProductsDemo.java
@@ -0,0 +1,335 @@
+import java.io.*;
+import java.util.*;
+
+public class ProductsDemo implements Serializable{
+    public static void main(String args[]) throws Exception {
+        int option = -1;
+        //s is used for Integer and s1 is for String data
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+        File file = new File("Products.txt");
+        ArrayList<Products> al = new ArrayList<>();
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        ListIterator li = null;
+
+        //check for existing file to load data to arraylist
+        if (file.isFile()) {
+            ois = new ObjectInputStream(new FileInputStream(file));
+            al = (ArrayList<Products>) ois.readObject();
+            ois.close();
+        }
+
+        do {
+            System.out.println("1.ADD PRODUCTS");
+            System.out.println("2.SHOW PRODUCTS");
+            System.out.println("3.UPDATE PRODUCTS");
+            System.out.println("4.SEARCH PRODUCTS (BY CATEGORY)");
+            System.out.println("5.DELETE PRODUCTS");
+            System.out.println("6.SORT PRODUCTS BY PRICE (Ascending)- On Screen only");
+            System.out.println("7.SORT PRODUCTS BY PRICE (Ascending)- In file");
+            System.out.println("8.SORT PRODUCTS BY ID (Ascending)- On Screen only");
+            System.out.println("9.SORT PRODUCTS BY ID (Ascending)- In file");
+            System.out.println("10.SORT PRODUCTS BY QUANTITY (Ascending)- On Screen only");
+            System.out.println("11.SORT PRODUCTS BY QUANTITY (Ascending)- In file");
+            System.out.println("0.EXIT");
+            System.out.println("Enter your option: ");
+            option = s.nextInt();
+        } while (option!=0);
+            switch (option) {
+                case 1:
+                    System.out.println("How many products do you want to add?");
+                    int amount = s.nextInt();
+                    for (int i = 0; i < amount; i++) {
+                        int id = 100 + i;
+                        System.out.print("Enter category:");
+                        String category = s1.next();
+
+                        System.out.print("Enter name:");
+                        String name = s1.next();
+
+                        System.out.print("Enter quantity:");
+                        int quantity = s.nextInt();
+
+                        System.out.print("Enter price:");
+                        int price = s.nextInt();
+
+                        Products products = new Products(id, category, name, quantity, price);
+                        al.add(products);
+                    }
+                    //write Objects into file
+                    oos = new ObjectOutputStream(new FileOutputStream(file));
+                    oos.writeObject(al);
+                    oos.close();
+                    break;
+                case 2:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 3:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+                        boolean found = false;
+                        System.out.println("Enter Product ID to Update:");
+                        int id = s1.nextInt();
+                        System.out.println("_________________________________________________");
+                        li = al.listIterator();
+                        while (li.hasNext()) {
+                            Products p = (Products) li.next();
+                            if (p.getId() == id) {
+                                System.out.print("Enter new product category: ");
+                                String category = s1.next();
+                                System.out.println("Enter new product name: ");
+                                String name = s1.next();
+                                System.out.print("Enter new product quantity: ");
+                                int quantity = s.nextInt();
+                                System.out.print("Enter new product price: ");
+                                int price = s.nextInt();
+                                li.set(new Products(id, category, name, quantity, price));
+                                found = true;
+                            }
+                        }
+                        if (found) {
+                            oos = new ObjectOutputStream(new FileOutputStream(file));
+                            oos.writeObject(al);
+                            oos.close();
+                            System.out.println("Record is successfully updated...!");
+                        } else {
+                            System.out.println("Records Not Found..!!!");
+                        }
+
+
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 4:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+                        boolean found = false;
+                        System.out.println("Enter Category to Search:");
+                        String category = s1.next();
+                        System.out.println("_________________________________________________");
+                        li = al.listIterator();
+                        while (li.hasNext()) {
+                            Products p = (Products) li.next();
+                            if (p.getCategory() == category) {
+                                System.out.println(p);
+                                found = true;
+                            }
+                        }
+                        if (!found)
+                            System.out.println("Records Not Found..!!!");
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 5:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+                        boolean found = false;
+                        System.out.print("Enter Product's name to Delete:");
+                        String name = s1.next();
+
+                        System.out.println("_________________________________________________");
+                        li = al.listIterator();
+                        while (li.hasNext()) {
+                            Products p = (Products) li.next();
+                            if (p.getName() == name) {
+                                li.remove();
+                                found = true;
+                            }
+                        }
+                        if (found) {
+                            oos = new ObjectOutputStream(new FileOutputStream(file));
+                            oos.writeObject(al);
+                            oos.close();
+                            System.out.println("Record is successfully deleted...!");
+                        } else {
+                            System.out.println("Records Not Found..!!!");
+                        }
+
+
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 6:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getPrice()- p2.getPrice();
+                            }
+                        });
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 7:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getPrice()- p2.getPrice();
+                            }
+                        });
+                        //This block of code will write the sorted data into file
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 8:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getId()- p2.getId();
+                            }
+                        });
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 9:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getId()- p2.getId();
+                            }
+                        });
+                        //This block of code will write the sorted data into file
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 10:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getQuantity()- p2.getQuantity();
+                            }
+                        });
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+                case 11:
+                    if (file.isFile()) {
+                        ois = new ObjectInputStream(new FileInputStream(file));
+                        al = (ArrayList<Products>) ois.readObject();
+                        ois.close();
+
+                        Collections.sort(al, new Comparator<Products>() {
+                            @Override
+                            public int compare(Products p1, Products p2) {
+
+                                return p1.getQuantity()- p2.getQuantity();
+                            }
+                        });
+                        //This block of code will write the sorted data into file
+                        oos = new ObjectOutputStream(new FileOutputStream(file));
+                        oos.writeObject(al);
+                        oos.close();
+
+                        System.out.println("_________________________________________________");
+                        //use ListIterator to iterate through the file
+                        li = al.listIterator();
+                        while (li.hasNext())
+                            System.out.println(li.next());
+                        System.out.println("_________________________________________________");
+                    } else {
+                        System.out.println("File Not Found...!!!");
+                    }
+                    break;
+            }
+    }
+}
