8e006b39f2b1363f7263cecc58c388367e2401a4
emma1 <emma.12030@outlook.com>
2022-08-18 16:28:50 +0700

    update the main so other can test the application

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to our shop! Click 1 if you already have an account, or 2 to register");
+
+        System.out.println("Login as an admin, choose 3");
+        System.out.println("<-------------------------------------->");
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.nextLine();
+        switch (userChoice) {
+            case ("1"):
+                login.customerLogin();
+                break;
+            case ("2"):
+                register.userRegister();
+                break;
+            case ("3"):
+                adminlogin.admLogin();
+                break;
+            default:
+                System.out.println("Invalid input");
+
+        }
     }
 }
