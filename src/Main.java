fdb7c9e51302c32d1e919699ecb7f777f2d657c4
emma1 <emma.12030@outlook.com>
2022-09-01 15:04:45 +0700

    update "my account"

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("COSC2081 GROUP ASSIGNMENT");
-        System.out.println("STORE ORDER MANAGEMENT SYSTEM");
-        System.out.println("Instructor: Mr. Minh Vu ");
-        System.out.println("Group: 16");
-        System.out.println("s3924821, Nhat Tran Minh ");
-        System.out.println("s3904418, Uyen Duong ");
-        System.out.println("s3904419, Phong Nguyen ");
+        System.out.println("COSC2081 GROUP ASSIGNMENT \n" +
+                "STORE ORDER MANAGEMENT SYSTEM \n" +
+                "Instructor: Mr. Minh Vu \n" +
+                "Group: Group 16 \n" +
+                "s3924821, Nhat Tran Minh \n" +
+                "s3904419, Phong Nguyen \n" +
+                "s3904418, Uyen Duong");
+        System.out.println("Welcome to our shop! Click 1 if you already have an account, or 2 to register");
+        System.out.println("Login as an admin, choose 3");
         System.out.println("<-------------------------------------->");
-
-        System.out.println("Welcome to our shop! Type 1 if you already have an account, or 2 to register");
-
-        System.out.println("Login as an admin, choose 3: ");
-
         Scanner scanner = new Scanner(System.in);
         String userChoice = scanner.nextLine();
         switch (userChoice) {
             case ("1"):
-                System.out.println("<-------------------------------------->");
                 MemberLogin.customerLogin();
                 break;
             case ("2"):
-                System.out.println("<-------------------------------------->");
                 RegisterDemo.registerMember();
                 break;
             case ("3"):
-                System.out.println("<-------------------------------------->");
                 loginAsAdmin.admLogin();
                 break;
             default:
--- src/myaccount.java
+++ src/myaccount.java
@@ -1,42 +1,38 @@
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
+import java.util.Arrays;
 
 public class myaccount {
 
     public static void userAccount(String userEmail) throws IOException {
-
-        String line;
-        String[] data;
-        while (userEmail.isEmpty()) {
-            System.out.println("You need to login first");
-            MemberLogin.customerLogin();
-
-        }
-        // newStatus(userEmail, data[5]);
-
         try {
+            String line;
+            String[] data;
+            while (userEmail.isEmpty()) {
+                System.out.println("You need to login first");
+                MemberLogin.customerLogin();
+
+            }
             FileReader fr = new FileReader("userdata.txt");
             BufferedReader br = new BufferedReader(fr);
+
             while ((line = br.readLine()) != null) {
+
                 data = line.split(",");
 
                 if (userEmail.equals(data[3])) {
-                    data = line.split(",");
-                    System.out.println("<--------------Your Account information-------------->");
-                    System.out.println("ID: " + data[0]);
-                    System.out.println("First name: " + data[1]);
-                    System.out.println("Last name: " + data[2]);
-                    System.out.println("Email: " + data[3]);
-                    String status = newStatus(data[5]);
+                    for (String s : Arrays.asList(
+                            "<--------------Your Account information-------------->",
+                            "ID: " + data[0], "First name: " + data[1], "Last name: " + data[2],
+                            "Email: " + data[3])) {
+                        System.out.println(s);
+                    }
+                    String status = newStatus(userEmail, data[5]);
                     System.out.println("Level of member: " + status);
-                    System.out.println("Total spending: " + data[6] + "VND");
+                    System.out.println("Total spending: " + data[6] + " VND");
                 }
 
             }
+
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
@@ -44,59 +40,53 @@ public class myaccount {
         }
     }
 
-    public static String newStatus(String userStatus) {
+    public static String newStatus(String userEmail, String userStatus) throws FileNotFoundException {
         float silverMember = 5000000;
         float goldMember = 10000000;
         float platinumMember = 25000000;
 
 
-        String line;
-        String[] data;
-
+        StringBuilder stringBuffer;
         try {
-
+            stringBuffer = new StringBuilder();
+            String line;
+            String[] data;
             FileReader fr = new FileReader("userdata.txt");
             BufferedReader br = new BufferedReader(fr);
 
-
             while ((line = br.readLine()) != null) {
                 data = line.split(",");
-                // if (userEmail.equals(data[3])) {
-//                if (Float.parseFloat(data[6]) < silverMember) {
-//                    userStatus = "New Member";
-//
-//                }
 
-                Path of = Path.of("userdata.txt");
-                if (Float.parseFloat(data[6]) > silverMember && Float.parseFloat(data[6]) < goldMember) {
-                    userStatus = data[5].replace(data[5], "Silver Member");
-//                    data[5] = userStatus;
+                if (userEmail.equals(data[3])) {
 
-                    Charset charset = StandardCharsets.UTF_8;
+                    if (Float.parseFloat(data[6]) < silverMember) {
+                        userStatus = "New Member";
+                        data[5] = userStatus;
 
-                    String content = Files.readString(of, charset);
-                    content = content.replaceAll(data[5], "Silver Member");
-                    Files.writeString(of, content, charset);
+                    } else if (Float.parseFloat(data[6]) > silverMember && Float.parseFloat(data[6]) < goldMember) {
+                        userStatus = data[5].replace(data[5], "Silver Member");
+                        data[5] = userStatus;
 
+                    } else if ((Float.parseFloat(data[6]) > goldMember && Float.parseFloat(data[6]) < platinumMember)) {
+                        userStatus = data[5].replace(data[5], "Gold Member");
+                        data[5] = userStatus;
 
-                } else if (Float.parseFloat(data[6]) > goldMember && Float.parseFloat(data[6]) < platinumMember) {
-                    userStatus = data[5].replace(data[5], "Gold Member");
-                    //                   data[5] = userStatus;
-                    Charset charset = StandardCharsets.UTF_8;
+                    } else if ((Float.parseFloat(data[6]) > platinumMember)) {
+                        userStatus = data[5].replace(data[5], "PlatinumMember");
+                        data[5] = userStatus;
 
-                    String content = Files.readString(of, charset);
-                    content = content.replaceAll(data[5], "Gold Member");
-                    Files.writeString(of, content, charset);
+                    }
+
+                    String row = data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5] + "," + data[data.length - 1];
+                    stringBuffer.append(row);
+                    stringBuffer.append(System.getProperty("line.separator"));
 
 
-                } else if (Float.parseFloat(data[6]) > platinumMember) {
-                    userStatus = data[5].replace(data[5], "Platinum Member");
-                    //                  data[5] = userStatus;
-                    Charset charset = StandardCharsets.UTF_8;
 
-                    String content = Files.readString(of, charset);
-                    content = content.replaceAll(data[5], "Platinum Member");
-                    Files.writeString(of, content, charset);
+                } else {
+
+                    stringBuffer.append(line);
+                    stringBuffer.append(System.getProperty("line.separator"));
 
                 }
 
@@ -104,10 +94,16 @@ public class myaccount {
 
             }
 
-            //           userStatus = userStatusBuilder.toString();
+            PrintWriter printWriter = new PrintWriter(new FileOutputStream("userdata.txt", false));
+            printWriter.print(stringBuffer);
+            printWriter.close();
+
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
+
         return userStatus;
-    }}
+    }
+}
