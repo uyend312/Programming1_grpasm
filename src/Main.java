59754bb5df8c18b6f306dbaa704e07feca988277
s3904419 <a8.phong06@gmail.com>
2022-09-12 18:29:54 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,24 +10,37 @@ public class Main {
                 "s3924821, Nhat Tran Minh \n" +
                 "s3904419, Phong Nguyen \n" +
                 "s3904418, Uyen Duong");
-        System.out.println("Welcome to our shop! Click 1 if you already have an account, or 2 to register");
-        System.out.println("Login as an admin, choose 3");
         System.out.println("<-------------------------------------->");
-        Scanner scanner = new Scanner(System.in);
-        String userChoice = scanner.nextLine();
-        switch (userChoice) {
-            case ("1"):
-                MemberLogin.customerLogin();
-                break;
-            case ("2"):
-                RegisterDemo.registerMember();
-                break;
-            case ("3"):
-                loginAsAdmin.admLogin();
-                break;
-            default:
-                System.out.println("Invalid input");
+        System.out.println("Welcome to our shop! Click 1 if you already have an account, or 2 to register.");
+        System.out.println("Login as an admin, choose 3.");
 
+        int userChoice = 0;
+
+        while(userChoice != 1 && userChoice != 2 && userChoice != 3) {
+            System.out.println("Enter your option 1-3:");
+            Scanner scanner = new Scanner(System.in);
+
+            //validate input must be integer
+            while (!scanner.hasNextInt()) {
+                System.out.println("INVALID INPUT!\nChoose an option 1-3: ");
+                scanner.next();
+            }
+
+            userChoice = scanner.nextInt();
+            switch(userChoice) {
+                case 1:
+                    MemberLogin.customerLogin();
+                    break;
+                case 2:
+                    RegisterDemo.registerMember();
+                    break;
+                case 3:
+                    loginAsAdmin.admLogin();
+                    break;
+                default:
+                    System.out.println("Invalid input " + userChoice);
+            }
         }
+        
     }
 }
